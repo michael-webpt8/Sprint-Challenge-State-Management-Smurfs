bc8c13d3c1d5867c0b5b797c358659055b2763c9
Mikey Williams <manicdesigner@gmail.com>
2019-10-26 18:41:07 -0400

    res.data in fetchSmurfs && remove dispatch to ADD_SMURFS

--- smurfs/src/components/actions/smurfs.js
+++ smurfs/src/components/actions/smurfs.js
@@ -4,15 +4,14 @@ export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_ERROR = 'FETCH_SMURFS_ERROR';
 export const ADD_SMURFS = 'ADD_SMURFS';
 
-export function fetchSmurfs(smurfs) {
+export function fetchSmurfs() {
 	return (dispatch) => {
 		dispatch({ type: FETCH_SMURFS_START });
 
 		axios
 			.get(`http://localhost:3333/smurfs`)
 			.then((res) => {
-				dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res });
-				dispatch({ type: ADD_SMURFS, payload: smurfs });
+				dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
 			})
 			.catch((err) => {
 				dispatch({ type: FETCH_SMURFS_ERROR, payload: err });
