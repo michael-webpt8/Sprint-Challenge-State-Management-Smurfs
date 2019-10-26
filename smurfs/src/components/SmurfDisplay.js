7692af233352bd644526e3fca517da3d4c4dc7be
Mikey Williams <manicdesigner@gmail.com>
2019-10-26 18:39:52 -0400

    destructure smurfs

--- smurfs/src/components/SmurfDisplay.js
+++ smurfs/src/components/SmurfDisplay.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 function SmurfDisplay(props) {
-	console.log(props.smurfs);
+	const { smurfs } = props;
+	console.log('loading', smurfs);
+
 	return (
-		<div>
-			{props.smurfs.map((smurf) => (
-				<div key={Date.now()}>
-					<h2>{smurf.name}</h2>
-					<p>{smurf.age}</p>
-					<p>{smurf.height}</p>
-				</div>
-			))}
-		</div>
+		<>
+			<div>
+				{smurfs.map((smurf) => (
+					<div key={Date.now()}>
+						<h2>{smurf.name}</h2>
+						<p>{smurf.age}</p>
+						<p>{smurf.height}</p>
+					</div>
+				))}
+			</div>
+		</>
 	);
 }
 export default SmurfDisplay;
