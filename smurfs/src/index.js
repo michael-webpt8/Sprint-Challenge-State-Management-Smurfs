40dfc315d1a90a608cc6f2aa1a60f09c268c2272
Mikey Williams <manicdesigner@gmail.com>
2019-10-26 18:48:15 -0400

    add middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer as smurfReducer } from './reducers/smurfs';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 const reducers = combineReducers({
 	smurfs: smurfReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
 	<Provider store={store}>
