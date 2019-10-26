332c3a446456b482d764b3554490bab1c4c282ba
Mikey Williams <manicdesigner@gmail.com>
2019-10-26 13:20:38 -0400

    smurfs for reducers

---
+++ smurfs/src/reducers/smurfs.js
@@ -0,0 +1,15 @@
+const initialState = [
+	{
+		name: 'Brainey',
+		age: 200,
+		height: '5cm',
+		id: 0
+	}
+];
+
+export function reducer(state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
