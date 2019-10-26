8dcf59de463039f32a17dcdb96fc38bd4b12505d
Mikey Williams <manicdesigner@gmail.com>
2019-10-26 13:36:43 -0400

    fixed payloads

--- smurfs/src/components/actions/smurfs.js
+++ smurfs/src/components/actions/smurfs.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_ERROR = 'FETCH_SMURFS_ERROR';
@@ -9,10 +10,10 @@ export function fetchSmurfs() {
 		axios
 			.get(`http://localhost:3333/smurfs`)
 			.then((res) => {
-				dispatch({ type: FETCH_SMURFS_SUCCESS, payload });
+				dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
 			})
 			.catch((err) => {
-				dispatch({ type: FETCH_SMURFS_ERROR, payload });
+				dispatch({ type: FETCH_SMURFS_ERROR, payload: err });
 			});
 	};
 }
