5a4ec6ef339b3ba31a9cee3d40baa8e88a7eb915
Michael Williams <manicdesigner@gmail.com>
2019-10-26 22:48:26 -0400

    rm console.logs

--- smurfs/src/reducers/smurfs.js
+++ smurfs/src/reducers/smurfs.js
@@ -1,46 +1,43 @@
 import {
-	FETCH_SMURFS_START,
-	FETCH_SMURFS_SUCCESS,
-	FETCH_SMURFS_ERROR,
-	ADD_SMURFS
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_ERROR,
+  ADD_SMURFS
 } from '../components/actions/smurfs';
 
 const initialState = {
-	smurfs: null,
-	getSmurfs: null,
-	isLoading: false,
-	error: null
+  smurfs: null,
+  getSmurfs: null,
+  isLoading: false,
+  error: null
 };
 
 export function reducer(state = initialState, action) {
-	switch (action.type) {
-		case FETCH_SMURFS_START:
-			console.log(action);
-			return {
-				...state,
-				isLoading: true
-			};
-		case FETCH_SMURFS_SUCCESS:
-			console.log(action);
-			return {
-				...state,
-				smurfs: action.payload,
-				isLoading: false
-			};
-		case FETCH_SMURFS_ERROR:
-			console.log(action);
-			return {
-				...state,
-				error: action.payload,
-				isLoading: false
-			};
-		case ADD_SMURFS:
-			return {
-				...state,
-				smurfs: state.smurfs.concat(state.getSmurfs),
-				isLoading: false
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false
+      };
+    case FETCH_SMURFS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case ADD_SMURFS:
+      return {
+        ...state,
+        smurfs: state.smurfs.concat(state.getSmurfs),
+        isLoading: false
+      };
+    default:
+      return state;
+  }
 }
