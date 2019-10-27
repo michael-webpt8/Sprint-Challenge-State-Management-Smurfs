bb013e82376d2cdbfdec3aceaada8aa8103596bd
Michael Williams <manicdesigner@gmail.com>
2019-10-27 13:08:57 -0400

    loading sign

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -5,38 +5,42 @@ import { connect } from 'react-redux';
 
 import SmurfDisplay from './SmurfDisplay';
 
-import { fetchSmurfs } from './actions/smurfs';
+import { fetchSmurfs } from '../actions/smurfs';
 
 function App(props) {
-	const { fetchSmurfs } = props;
-	useEffect(() => {
-		fetchSmurfs();
-	}, [fetchSmurfs]);
+  const { fetchSmurfs } = props;
+  useEffect(() => {
+    fetchSmurfs();
+  }, [fetchSmurfs]);
 
-	if (!props.smurfs) {
-		return <h2>Loading...</h2>;
-	}
-	return (
-		<div className="App">
-			<h1>SMURFS! 2.0 W/ Redux</h1>
-			<div>Welcome to your state management version of Smurfs!</div>
-			<SmurfForm />
-			<SmurfDisplay smurfs={props.smurfs} />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <SmurfForm />
+      {props.error && <p>Error: {props.error}</p>}
+      {props.isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <SmurfDisplay smurfs={props.smurfs} />
+      )}
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-	return {
-		smurfs: state.smurfs.smurfs
-	};
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs.smurfs,
+    error: state.smurfs.error,
+    isLoading: state.smurfs.isLoading
+  };
 };
 
 const mapDispatchToProps = {
-	fetchSmurfs
+  fetchSmurfs
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(App);
