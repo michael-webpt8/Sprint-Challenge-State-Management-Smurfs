cab7b1d8f019837266e720975405fe3ce22189c1
Michael Williams <manicdesigner@gmail.com>
2019-10-27 13:09:35 -0400

    loading

--- smurfs/src/components/SmurfDisplay.js
+++ smurfs/src/components/SmurfDisplay.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
 function SmurfDisplay(props) {
-	const { smurfs } = props;
-	console.log('loading', smurfs);
-
-	return (
-		<>
-			<div>
-				{smurfs.map((smurf) => (
-					<div key={Date.now()}>
-						<h2>{smurf.name}</h2>
-						<p>{smurf.age}</p>
-						<p>{smurf.height}</p>
-					</div>
-				))}
-			</div>
-		</>
-	);
+  const { smurfs } = props;
+  console.log('loading', smurfs);
+  if (!props.smurfs) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      <div>
+        {smurfs.map(smurf => (
+          <div key={Date.now()}>
+            <h2>{smurf.name}</h2>
+            <p>{smurf.age}</p>
+            <p>{smurf.height}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 export default SmurfDisplay;
