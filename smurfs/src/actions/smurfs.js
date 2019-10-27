0a674987b13f4c15c6169c0668134e4e029f2fd8
Michael Williams <manicdesigner@gmail.com>
2019-10-27 12:30:42 -0400

    altered how to get smurfs

--- smurfs/src/actions/smurfs.js
+++ smurfs/src/actions/smurfs.js
@@ -2,20 +2,29 @@ import axios from 'axios';
 export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_ERROR = 'FETCH_SMURFS_ERROR';
+export const FETCH_SMURFS = 'FETCH_SMURFS';
 
 export const ADD_SMURFS_START = 'ADD_SMURFS_START';
 export const ADD_SMURFS_ADD = 'ADD_SMURFS_ADD';
 export const ADD_SMURFS_SUCCESS = 'ADD_SMURFS_SUCCESS';
 export const ADD_SMURFS_ERROR = 'ADD_SMURFS_ERROR';
 
+const apiUrl = `http://localhost:3333/smurfs`;
+
+export const getSmurfs = smurfs => {
+  return {
+    type: FETCH_SMURFS,
+    smurfs
+  };
+};
+
 export function fetchSmurfs() {
   return dispatch => {
     dispatch({ type: FETCH_SMURFS_START });
-
     axios
-      .get(`http://localhost:3333/smurfs`)
+      .get(apiUrl)
       .then(res => {
-        dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+        dispatch(getSmurfs(res.data));
       })
       .catch(err => {
         dispatch({ type: FETCH_SMURFS_ERROR, payload: err });
@@ -23,16 +32,26 @@ export function fetchSmurfs() {
   };
 }
 
-export function addSmurf(smurf) {
+export function addSmurf({ name, height, age }) {
   return dispatch => {
-    dispatch({ type: ADD_SMURFS_START });
-
-    axios
-      .post(`http://localhost:3333/smurfs`, {
-        type: ADD_SMURFS_ADD,
-        payload: smurf
+    return axios
+      .post(`http://localhost:3333/smurfs`, { name, height, age })
+      .then(res => {
+        dispatch(smurfAddSucces(res.data));
       })
-      .then(res => ({ type: ADD_SMURFS_SUCCESS, payload: res.data }))
-      .catch(err => ({ type: ADD_SMURFS_ERROR, payload: err }));
+      .catch(err => {
+        dispatch({ type: ADD_SMURFS_ERROR, payload: err });
+      });
   };
 }
+
+export const smurfAddSucces = data => {
+  return {
+    type: ADD_SMURFS_ADD,
+    payload: {
+      name: data.name,
+      height: data.height,
+      age: data.age
+    }
+  };
+};
--- smurfs/src/reducers/smurfs.js
+++ smurfs/src/reducers/smurfs.js
@@ -1,29 +1,63 @@
 import {
   FETCH_SMURFS_START,
   FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS,
   FETCH_SMURFS_ERROR,
-  ADD_SMURFS
-} from '../components/actions/smurfs';
+  ADD_SMURFS_START,
+  ADD_SMURFS_SUCCESS,
+  ADD_SMURFS_ADD,
+  ADD_SMURFS_ERROR
+} from '../actions/smurfs';
 
 const initialState = {
   smurfs: null,
-  getSmurfs: null,
+  smurf: null,
+  //  getSmurfs: null,
   isLoading: false,
   error: null
 };
 
 export function reducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_SMURFS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case ADD_SMURFS_ADD:
+      return {
+        ...state,
+        smurfs: state.smurfs.concat({ smurf: action.payload }),
+        isLoading: false
+      };
+    case ADD_SMURFS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        smurfs: action.payload
+      };
+    case ADD_SMURFS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
     case FETCH_SMURFS_START:
       return {
         ...state,
         isLoading: true
       };
-    case FETCH_SMURFS_SUCCESS:
+    // case FETCH_SMURFS_SUCCESS:
+    //   return {
+    //     ...state,
+    //     smurfs: action.payload,
+    //     isLoading: false
+    //   };
+    case FETCH_SMURFS:
       return {
         ...state,
-        smurfs: action.payload,
-        isLoading: false
+        isLoading: false,
+        smurfs: action.smurfs
       };
     case FETCH_SMURFS_ERROR:
       return {
@@ -31,12 +65,7 @@ export function reducer(state = initialState, action) {
         error: action.payload,
         isLoading: false
       };
-    case ADD_SMURFS:
-      return {
-        ...state,
-        smurfs: state.smurfs.concat(state.getSmurfs),
-        isLoading: false
-      };
+
     default:
       return state;
   }
