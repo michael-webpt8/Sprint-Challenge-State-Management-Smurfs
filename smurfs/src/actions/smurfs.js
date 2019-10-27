d36f0fa5d5f4cf14fc21f60c65d98d4ca2f6964a
Michael Williams <manicdesigner@gmail.com>
2019-10-26 23:14:27 -0400

    add post smurf action

--- smurfs/src/actions/smurfs.js
+++ smurfs/src/actions/smurfs.js
@@ -2,25 +2,37 @@ import axios from 'axios';
 export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_ERROR = 'FETCH_SMURFS_ERROR';
-export const ADD_SMURFS = 'ADD_SMURFS';
+
+export const ADD_SMURFS_START = 'ADD_SMURFS_START';
+export const ADD_SMURFS_ADD = 'ADD_SMURFS_ADD';
+export const ADD_SMURFS_SUCCESS = 'ADD_SMURFS_SUCCESS';
+export const ADD_SMURFS_ERROR = 'ADD_SMURFS_ERROR';
 
 export function fetchSmurfs() {
-	return (dispatch) => {
-		dispatch({ type: FETCH_SMURFS_START });
+  return dispatch => {
+    dispatch({ type: FETCH_SMURFS_START });
 
-		axios
-			.get(`http://localhost:3333/smurfs`)
-			.then((res) => {
-				dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
-			})
-			.catch((err) => {
-				dispatch({ type: FETCH_SMURFS_ERROR, payload: err });
-			});
-	};
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(res => {
+        dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+      })
+      .catch(err => {
+        dispatch({ type: FETCH_SMURFS_ERROR, payload: err });
+      });
+  };
 }
 
 export function addSmurf(smurf) {
-	return (dispatch) => {
-		dispatch({ type: ADD_SMURFS, payload: smurf });
-	};
+  return dispatch => {
+    dispatch({ type: ADD_SMURFS_START });
+
+    axios
+      .post(`http://localhost:3333/smurfs`, {
+        type: ADD_SMURFS_ADD,
+        payload: smurf
+      })
+      .then(res => ({ type: ADD_SMURFS_SUCCESS, payload: res.data }))
+      .catch(err => ({ type: ADD_SMURFS_ERROR, payload: err }));
+  };
 }
