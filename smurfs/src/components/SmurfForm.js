6952335fe7f72894473f2a0b0bd6c70dc0bab811
Michael Williams <manicdesigner@gmail.com>
2019-10-27 13:09:18 -0400

    getSmurfs dispatch

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,58 +1,68 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { getSmurfs } from '../actions/smurfs';
 
-function SmurfForm() {
-	const [name, setName] = useState('');
-	const [age, setAge] = useState(0);
-	const [height, setHeight] = useState('');
+function SmurfForm(props) {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState('');
 
-	const handleName = (e) => {
-		setName(e.target.value);
-	};
-	const handleAge = (e) => {
-		setAge(e.target.value);
-	};
-	const handleHeight = (e) => {
-		setHeight(e.target.value);
-	};
+  const handleName = e => {
+    setName(e.target.value);
+  };
+  const handleAge = e => {
+    setAge(e.target.value);
+  };
+  const handleHeight = e => {
+    setHeight(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.getSmurfs({ name, age, height });
 
-		setName('');
-		setAge(0);
-		setHeight('');
-	};
+    setName('');
+    setAge(0);
+    setHeight('');
+  };
 
-	return (
-		<>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="input"
-					name="smurfName"
-					onChange={handleName}
-					value={name}
-					placeholder="Smurfy name..."
-				/>
-				<br />
-				<input
-					type="number"
-					name="smurfAge"
-					onChange={handleAge}
-					value={age}
-					placeholder="Smurfs age..."
-				/>
-				<br />
-				<input
-					type="input"
-					name="smurfHeight"
-					onChange={handleHeight}
-					value={height}
-					placeholder="Smurfs Height..."
-				/>
-				<button type="submit">Add Smurf</button>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='input'
+          name='smurfName'
+          onChange={handleName}
+          value={name}
+          placeholder='Smurfy name...'
+        />
+        <br />
+        <input
+          type='number'
+          name='smurfAge'
+          onChange={handleAge}
+          value={age}
+          placeholder='Smurfs age...'
+        />
+        <br />
+        <input
+          type='input'
+          name='smurfHeight'
+          onChange={handleHeight}
+          value={height}
+          placeholder='Smurfs Height...'
+        />
+        <button type='submit'>Add Smurf</button>
+      </form>
+    </>
+  );
 }
-export default connect()(SmurfForm);
+
+const mapDispatchToProps = {
+  getSmurfs
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SmurfForm);
